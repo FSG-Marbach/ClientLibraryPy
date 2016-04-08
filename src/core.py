d1b19226f8261d38c6d6ba53881d5418b177c139
cale <cdqwertz@gmail.com>
2016-04-08 16:45:54 +0200

    uploaded

--- src/core.py
+++ src/core.py
@@ -1,5 +1,20 @@
+import socket
+
 class SasClient:
-    def __init__(self):
-        pass
+    def __init__(self, serverIP, serverPort):
+        self.serverIP = serverIP
+        self.serverPort = serverPort
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect((self.serverIP, self.serverPort))
+    
     def send(self, txt):
-        pass
+        self.sock.send(txt)
+
+    def recv(self):
+        return self.sock.recv(2048)
+
+    def close(self):
+        self.sock.shutdown()
+        self.sock.close()
+
+
